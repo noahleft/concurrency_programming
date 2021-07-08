bcd9f4be06147705a4b11e99be8bc63e96770770
ShihFengLin <noahleft@gmail.com>
2021-07-09 00:40:15 +0800

    rewrite the threading data to prevent the data race

--- prime_number/sieve.cpp
+++ prime_number/sieve.cpp
@@ -47,9 +47,15 @@ int cal_prime_number_under_n(int n) {
 #elif defined(sieve_v3)
 #include <thread>
 #define NUM_THREADS 4
-vector<bool> prime_vec;
 
-void filterout_composite_number(int base) {
+typedef struct thread_param {
+    vector<bool> prime_vec;
+    int base;
+} tparam_t;
+
+void filterout_composite_number(tparam_t *pArgs) {
+    vector<bool> &prime_vec = pArgs->prime_vec;
+    int base = pArgs->base;
     for(; base<prime_vec.size(); base+=NUM_THREADS) {
         if(!prime_vec[base]) continue;
         for(int j=2*base; j<prime_vec.size(); j+=base) {
@@ -59,18 +65,31 @@ void filterout_composite_number(int base) {
 }
 
 int cal_prime_number_under_n(int n) {
-    prime_vec.resize(n+1, true);
-    prime_vec[0] = false;
-    prime_vec[1] = false;
 
     vector<thread> tHandles(NUM_THREADS);
+    vector<tparam_t> tParams(NUM_THREADS);
 
     for(int i=0; i<NUM_THREADS; i++) {
-        tHandles[i] = thread(filterout_composite_number, i);
+        tParams[i].prime_vec.resize(n+1, true);
+        tParams[i].prime_vec[0] = false;
+        tParams[i].prime_vec[1] = false;
+        tParams[i].base = i;
+        tHandles[i] = thread(filterout_composite_number, &tParams[i]);
     }
 
     for(int j=0; j<NUM_THREADS; j++) {
         tHandles[j].join();
+    }
+
+    vector<bool> prime_vec;
+    prime_vec.resize(n+1, true);
+    for(int i=0; i<prime_vec.size(); i++) {
+        for(int j=0; j<NUM_THREADS; j++) {
+            if(!tParams[j].prime_vec[i]) {
+                prime_vec[i] = false;
+                break;
+            }
+        }
     }
 
     int num = 0;
